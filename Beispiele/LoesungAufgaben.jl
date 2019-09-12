94740a7b62df0a150de615472b34f3c63c3e1099
Sebastian Pech <sebastian.pech@me.com>
2019-09-12 15:06:04 +0200

    Add more exercises

--- Beispiele/LoesungAufgaben.jl
+++ Beispiele/LoesungAufgaben.jl
@@ -1,5 +1,37 @@
 using JuliaSkriptumKontrolle
 
+@Aufgabe "10.3.1" begin
+    using LinearAlgebra
+
+    function lösbar(A,b)
+        rank(A) == rank(hcat(A,b))
+    end
+end
+
+@Aufgabe "10.3.2" begin
+    using LinearAlgebra
+    A = [1 -1 1
+        2 3 0
+        10 -0.5 5
+        ]
+    b = [22,11, 0]
+    A\b
+end
+
+@Aufgabe "10.3.3" begin
+    using LinearAlgebra
+    function rekonstruiere(λ,ϕ)
+        ϕ*Diagonal(λ)*inv(ϕ)
+    end
+end
+
+@Aufgabe "10.3.4" begin
+    using LinearAlgebra
+    function winkel(v1,v2)
+        acos(v1⋅v2/(norm(v1)*norm(v2)))
+    end
+end
+
 @Aufgabe "11.3.1" function gerade_ungerade(x::Int)
     if x % 2 == 0
         println("Gerade")
@@ -56,13 +88,6 @@ end
 
 @Aufgabe "11.3.5.2" begin
     throw() = rand(1:6)
-    function P_est_1(n)
-        sum([all(i->i==4,(throw() for _ in 1:3)) for __ in 1:n])/n
-    end
-end
-
-@Aufgabe "11.3.5.2" begin
-    throw() = rand(1:6)
     function P_est_2(n)
         sum([any(i->i==4,(throw() for _ in 1:3)) for __ in 1:n])/n
     end
@@ -91,9 +116,24 @@ end
 end
 
 @Aufgabe "11.3.8" function dict_merge(d1, d2)
-    k = collect(keys(d1))
-    append!(k,collect(keys(d2)))
-    v = collect(values(d1))
-    append!(v,collect(values(d2)))
-    return Dict(zip(k,v))
+    d = copy(d1)
+    for k in keys(d2)
+        d[k] = d2[k]
+    end
+    return d
 end
+
+@Aufgabe "14.3.1" begin
+    using Dates
+    isdate(x,y) = Dates.value(Day(x)) == Dates.value(Month(y)) && Dates.value(Day(y)) == Dates.value(Month(x))
+
+    function get_date()
+        for d in range(Date(2017,1,1),Date(2017,12,31),step=Day(1))
+            if isdate(d,d+Week(6))
+                return d+Week(6)
+            end
+        end
+    end
+end
+
+JuliaSkriptumKontrolle.status()
